c2328a1e51dc859ecbae2a1413d90aebbd33070a
Matt Carlson <matthewthomascarlson@gmail.com>
2017-09-14 18:30:25 -0700

    make API request in ActionCreator

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '89f17c12732050af0fecc7fed7d43c39';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
